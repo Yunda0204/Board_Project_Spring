df747eb8729917d9d3296c76acf2cdcdd072734e
Yunda0204 <a01034353045@gmail.com>
2024-08-07 14:58:34 +0900

    kakao map API 넣는중

---
+++ src/main/java/org/choongang/tour/controllers/TourController.java
@@ -0,0 +1,24 @@
+package org.choongang.tour.controllers;
+
+import org.choongang.global.exceptions.ExceptionProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/tour")
+public class TourController implements ExceptionProcessor {
+
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable Long seq, Model model) {
+
+        model.addAttribute("CommonScript", List.of("map"));
+        model.addAttribute("addScript", List.of("tour/view"));
+
+        return "front/tour/view";
+    }
+}
